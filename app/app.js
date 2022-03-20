017eff6fb1cbae89af5a377deb835846a7ff3088
Christopher Stone <cmstone@student.fullsail.edu>
2022-03-20 14:49:50 -0400

    Add middleware modules

---
+++ app/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+// middleware for logging
+app.use(morgan("dev"));
+
+// middleware for parsing
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// middleware that all request are json
+app.use(express.json());
+
+// middleware for CORS Policy
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "POST, PUT, GET, PATCH, DELETE");
+    };
+
+    next();
+})
+
+// middleware for error handling
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND!!!");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message, 
+            status: error.status,
+            method: req.method
+        }
+    });
+});
+
+module.exports = app;
